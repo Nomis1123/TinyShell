1ebc995edb311023156a8991f99d21161a490c20
Nomis1123 <72155251+Nomis1123@users.noreply.github.com>
2025-06-05 19:51:43 -0400

    echo and ls started

--- src/builtins.c
+++ src/builtins.c
@@ -18,14 +18,9 @@ bn_ptr check_builtin(const char *cmd) {
 }
 
 
-// ===== Builtins =====
-
-/* Prereq: tokens is a NULL terminated sequence of strings.
- * Return 0 on success and -1 on error ... but there are no errors on echo. 
- */
+// ====== echo =====
 ssize_t bn_echo(char **tokens) {
     ssize_t index = 1;
-
     if (tokens[index] != NULL) {
         display_message(tokens[index]);
         index += 1;
@@ -36,6 +31,55 @@ ssize_t bn_echo(char **tokens) {
         index += 1;
     }
     display_message("\n");
-
     return 0;
+}
+
+
+// ====== ls =====
+static void list_directory(const char *path, LsOptions options, int current_depth) {
+
+    DIR *dir = opendir(path);
+    if (!dir) {
+        display_error("ERROR: Invalid path", "");
+        display_error("ERROR: Builtin failed: ", "ls");
+
+        return;
+    }
+
+    char *dirs [1024];
+    int dir_count = 0;
+
+    struct dirent *entry;
+    while ((entry = readdir(dir)) != NULL) {
+        
+
+        if (options.filter && !strstr(entry->d_name, options.filter)) continue;
+
+        display_message(entry->d_name);
+        display_message(" ");
+        display_message("\n");
+        
+        if (entry->d_type == DT_DIR && strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
+            dirs[dir_count] = entry->d_name;
+            dir_count++;
+        }
+
+        
+    }
+
+    for (int count = 0; count < dir_count; count++) 
+    {
+        if (options.recursive && (options.depth == -1 || current_depth < options.depth)) {
+            char full_path[1024];
+            snprintf(full_path, sizeof(full_path), "%s/%s", path, dirs[count]);
+
+            struct stat st;
+            if (stat(full_path, &st) == 0 && S_ISDIR(st.st_mode)) {
+                list_directory(full_path, options, current_depth + 1);
+            }
+        }
+    }
+        
+    closedir(dir);
+
 }
