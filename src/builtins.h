f3078bd3149213f1a6268e8745c42002faabebe0
Nomis1123 <72155251+Nomis1123@users.noreply.github.com>
2025-06-05 19:53:57 -0400

    updated header file for builtins

--- src/builtins.h
+++ src/builtins.h
@@ -2,6 +2,11 @@
 #define __BUILTINS_H__
 
 #include <unistd.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include "variables.h"
+#include "io_helpers.h"
 
 
 /* Type for builtin handling functions
@@ -10,17 +15,24 @@
  */
 typedef ssize_t (*bn_ptr)(char **);
 ssize_t bn_echo(char **tokens);
-
-
-/* Return: index of builtin or -1 if cmd doesn't match a builtin
- */
+ssize_t bn_ls(char **tokens);
+ssize_t bn_cd(char **tokens); 
+ssize_t bn_cat(char **tokens); 
+ssize_t bn_wc(char **tokens); 
+ 
 bn_ptr check_builtin(const char *cmd);
 
-
-/* BUILTINS and BUILTINS_FN are parallel arrays of length BUILTINS_COUNT
- */
-static const char * const BUILTINS[] = {"echo"};
-static const bn_ptr BUILTINS_FN[] = {bn_echo, NULL};    // Extra null element for 'non-builtin'
+static const char * const BUILTINS[] = {"echo", "ls", "cd", "cat", "wc"};
+static const bn_ptr BUILTINS_FN[] = {&bn_echo, &bn_ls, &bn_cd, &bn_cat, &bn_wc};    
 static const ssize_t BUILTINS_COUNT = sizeof(BUILTINS) / sizeof(char *);
+
+/* LS options struct */
+typedef struct {
+    int recursive;
+    int depth;
+    char *filter;
+    char *path;
+} LsOptions;
+
 
 #endif
